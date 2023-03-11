32a09240885df2b47cc672d418f80327b792377d
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2023-03-11 23:32:26 +0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+let snake = document.getElementById('snake');
+let food = document.getElementById('food');
+
+let x = 200;
+let y = 200;
+let speed = 10;
+let direction = 'right';
+
+function moveSnake() {
+    if (direction === 'right') {
+        x += speed;
+    } else if (direction === 'left') {
+        x -= speed;
+    } else if (direction === 'up') {
+        y -= speed;
+    } else if (direction === 'down') {
+        y += speed;
+    }
+
+    snake.style.left = x + 'px';
+    snake.style.top = y + 'px';
+}
+
+function changeDirection(event) {
+    if (event.keyCode === 37) {
+        direction = 'left';
+    } else if (event.keyCode === 38) {
+        direction = 'up';
+    } else if (event.keyCode === 39) {
+        direction = 'right';
+    } else if (event.keyCode === 40) {
+        direction = 'down';
+    }
+}
+
+document.addEventListener('keydown', changeDirection);
+
+setInterval(function() {
+    moveSnake();
+}, 100);
